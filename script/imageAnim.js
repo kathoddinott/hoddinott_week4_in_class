fc7c1d6fcdcf4798171a415c44528785dcc209c5
Hoddinott <k_hoddinott2@tss-hr420-dm15.fc.ca>
2019-01-28 16:21:58 -0500

    secondcommit

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,11 +1,26 @@
 (() => {
-	let theButton = document.querySelector("#buttonHolder img");
-	window.addEventListener("load", changeHeaderText);
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	function changeHeaderText() {
-		document.querySelector("h1").textContent = "hey there from js"
-	}
-	// set up the puzzle pieces and boards
-	theButton.addEventListener("click", changeHeaderText);
-	
+	let piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector("puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+		function createPuzzlePieces() {
+			//debugger;
+			pieces.forEach((piece, index) => {
+				let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/$(piece + pictureIndex).jpg" alt="thumbnail"
+
+				piecesBoard.innerHTML +- newPuzzlePiece;
+			});
+		}
+
+		function resetPuzzlePieces() {
+			//debugger;
+		}
+
+		puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces))
+
+
+		createPuzzlePieces(0);
+
 })();
